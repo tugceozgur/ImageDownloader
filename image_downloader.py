42bd36799f68c476d5f2488987b92f42249d8623
tugceozgur <tugceoztetik@gmail.com>
2019-05-16 23:08:48 +0200

    save images by their name

--- image_downloader.py
+++ image_downloader.py
@@ -1,9 +1,10 @@
+import os 
 import requests
 with open("url_links.txt", "r") as urls:
     data = urls.read().splitlines()
     for i,line in enumerate(data):
-        image_name = str(i).zfill(8)
-        urls = open(image_name + ".jpg", "wb")
-        urls.write(requests.get(line).content)
-    f.close()  
+        image_name = os.path.basename(line)
+        image = open(image_name, "wb")
+        image.write(requests.get(line).content)
+    image.close()  
 
