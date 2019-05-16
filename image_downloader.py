3d24bcad015fe4ed714fc84685a92ee4647d60b5
tugceozgur <tugceoztetik@gmail.com>
2019-05-16 22:49:48 +0200

    download images

--- image_downloader.py
+++ image_downloader.py
@@ -1,5 +1,9 @@
-with open('url_links.txt', 'r') as f:
-    for line in f: 
-        print(line)
-        
+import requests
+with open("url_links.txt", "r") as urls:
+    data = urls.read().splitlines()
+    for i,line in enumerate(data):
+        image_name = str(i).zfill(8)
+        urls = open(image_name + ".jpg", "wb")
+        urls.write(requests.get(line).content)
+    f.close()  
 
