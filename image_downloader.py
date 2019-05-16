6ae32343295d2b9285078b94eeade14049929800
tugceozgur <tugceoztetik@gmail.com>
2019-05-17 00:25:34 +0200

    Handle general and specific errors

--- image_downloader.py
+++ image_downloader.py
@@ -2,17 +2,21 @@ import os
 import requests
 import datetime
 
-def logging(line_number):
-    print(str(datetime.datetime.now()) + " invalid address in "  + str(line_number) +" line.")
+def logging(log_message):
+    with open("logs.txt", "a+") as log:
+        log.write(str(datetime.datetime.now()) + " " + log_message + "\n")
 
 with open("url_links.txt", "r") as urls:
     data = urls.read().splitlines()
-    for i,line in enumerate(data):
-        if len(line) < 5:
-            logging(i)
-            continue
-        image_name = os.path.basename(line)
-        image = open("images/" + image_name, "wb")
-        image.write(requests.get(line).content)
+    for line_number,line in enumerate(data):
+        try:
+            assert len(line) > 5, "Image URL is too short"
+            image_name = os.path.basename(line)
+            image = open("images/" + image_name, "wb")
+            image.write(requests.get(line).content)
+        except AssertionError as err_msg:
+            logging(str(err_msg) + " at line " + str(line_number))
+        except Exception as generic_err:
+            logging(str(generic_err))
     image.close()  
 
