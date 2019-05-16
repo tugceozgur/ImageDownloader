f1ef62f2285b8bfc40374a8c2170d4f99f18c99b
tugceozgur <tugceoztetik@gmail.com>
2019-05-16 23:33:02 +0200

    give error for address length smaller than 5

--- image_downloader.py
+++ image_downloader.py
@@ -1,10 +1,18 @@
 import os 
 import requests
+import datetime
+
+def logging(line_number):
+    print(str(datetime.datetime.now()) + " invalid address in "  + str(line_number) +" line.")
+
 with open("url_links.txt", "r") as urls:
     data = urls.read().splitlines()
     for i,line in enumerate(data):
+        if len(line) < 5:
+            logging(i)
+            continue
         image_name = os.path.basename(line)
-        image = open(image_name, "wb")
+        image = open("images/" + image_name, "wb")
         image.write(requests.get(line).content)
     image.close()  
 
