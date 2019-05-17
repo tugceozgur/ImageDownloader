fe72591992083cf5c9348011e9a79e322d9967b8
tugceozgur <tugceoztetik@gmail.com>
2019-05-17 18:34:39 +0200

    take plaintext file as argument

--- image_downloader.py
+++ image_downloader.py
@@ -2,6 +2,7 @@ import os
 import requests
 import datetime
 import configurations as conf
+import sys
 
 def logging(log_message):
     with open("logs.txt", "a+") as log:
@@ -13,10 +14,11 @@ def download_image(image_url,line_number):
     image_format = response.headers['Content-Type'].split("/")[1]
     image_name = str(line_number+1)+ "." +image_format
     image_path ="images/" + image_name
-    image = open(image_path,"wb")
-    image.write(response.content)
+
+    with open(image_path,"wb") as img:
+        img.write(response.content)
+
     image_size = os.path.getsize(image_path)
-    image.close()
     assert image_size > conf.min_image_size, ("Image size is too small at " 
                                                 + str(line_number) + " \n\tURL is " + line)
     return image_path
@@ -25,15 +27,20 @@ if __name__ == '__main__':
     directory = "images/"
     if not os.path.exists(directory):
         os.makedirs(directory)
-
-    with open("url_links.txt", "r") as urls:
+    #takes plaintext file as argument
+    with open(sys.argv[1], "r") as urls:
         data = urls.read().splitlines()
+
         for line_number,line in enumerate(data):
             try:
+
                 assert len(line) > conf.min_line_length, "Image URL is too short" 
                 download_image(line,line_number)
+
             except AssertionError as err_msg:
                 logging(str(err_msg) + " at line " + str(line_number))
+
             except Exception as generic_err:
                 logging(str(generic_err))
 
+            
